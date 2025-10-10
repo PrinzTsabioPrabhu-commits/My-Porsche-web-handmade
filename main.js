a19849989bf4727dbc5c4ca1371080957af115d9
Mas Tsabio <prinztsabioprabhudanishwara@gmail.com>
2025-10-10 21:41:31 +0700

    change name html

--- main.js
+++ main.js
@@ -43,3 +43,15 @@
     // Menambahkan Event Listener: Ketika tombol diklik, panggil fungsi picuAnimasi
     tombol.addEventListener('click', picuAnimasi);
 });
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+      setTimeout(() => {
+        document.getElementById("puzzle-loader").classList.add("fade-out");
+        setTimeout(() => {
+          document.getElementById("puzzle-loader").style.display = "none";
+          document.body.style.overflow = "auto";
+        }, 1000);
+      }, 6000);
+    });
